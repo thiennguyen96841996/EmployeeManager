d0b4889c53f98a91861f040730c97fc80a457a49
thien nguyen <thiennh@haposoft.com>
2018-03-19 12:19:31 +0700

    create migration, model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,14 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('address')->nullable();
+            $table->date('birthday')->nullable();
+            $table->text('avatar')->nullable();
+            $table->string('sex')->nullable();
+            $table->string('level_japanese')->nullable();
+            $table->string('level_employee')->nullable();
+            $table->text('department')->nullable();
+            $table->SoftDeletes();
             $table->rememberToken();
             $table->timestamps();
         });
